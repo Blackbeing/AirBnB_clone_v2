e7764d48fe0a28bce5947575f1231b2f178b21ef
Blackbeing <johnjahi55@gmail.com>
2023-07-06 19:58:41 +0000

    feat Use fabric to create tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Fabric module to compress folder to tgz archive for deployment
+"""
+
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """
+    Compress folder to tgz archive
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(now)
+
+    # Make version directory
+    local("mkdir -p versions")
+    result = local(
+        "tar -cvzf versions/{} {}".format(archive_name, "web_static"),
+    )
+    if result.failed:
+        return None
+    return "versions/{}".format(archive_name)
